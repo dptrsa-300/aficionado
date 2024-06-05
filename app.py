d0a853f99a5ddc145d86c61b9c043ec1a21c4cd4
danie.theron <danie.theron@gmail.com>
2024-06-05 16:30:45 +0000

    flatten

--- app.py
+++ app.py
@@ -119,7 +119,8 @@ with st.container(border=True):
     enc_email = encrypt(st.text_input(label='Email Address').encode(encoding="utf-8"))
     enc_pwd = encrypt(st.text_input(label='Password', type='password').encode(encoding="utf-8"))
     if st.button(label='Login'):
-        response = call_cloud_function({"credentials": {'email': enc_email, 'password': enc_pwd}, 
+        response = call_cloud_function({'email': enc_email, 
+                                        'password': enc_pwd, 
                                         'key': st.secrets['GCF_API_KEY'],
                                         }, st.secrets['GCF_ENDPOINTS']['authenticate'])
         st.write(response)
